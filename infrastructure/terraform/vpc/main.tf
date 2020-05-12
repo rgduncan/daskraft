80a8c0e2e3ff6751bc2a7e8b11e821663db5e41d
rgduncan <robert.duncan.iv@gmail.com>
2020-05-12 00:31:23 -0700

    add s3 backend

--- infrastructure/terraform/vpc/main.tf
+++ infrastructure/terraform/vpc/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend s3 {
+    bucket = "rgduncan-terraform-state"
+    key = "infrastructure/vpc.tfstate"
+    region = "us-west-2"
+  }
+}
+
 provider "aws" {
   region = "us-west-2"
   shared_credentials_file = "~/.aws/credentials"
